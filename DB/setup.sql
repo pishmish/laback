0155d94ab494705f461a59d27ce02f22152806c0
Musab Ahmed Khan <83080859+MAKhan22@users.noreply.github.com>
2024-11-01 23:58:10 +0300

    added a ton of tables

--- DB/setup.sql
+++ DB/setup.sql
@@ -2,6 +2,29 @@ create database if not exists `zadados`;
 
 use zadados;
 
+create table `Address` (
+	`addressID` int not null auto_increment unique,
+	`addressTitle` varchar(64) not null,
+	`country` varchar(64) not null,
+	`city` varchar(64) not null,
+	`province` varchar(64),
+	`zipCode` varchar(10) not null,
+	`streetAddress` varchar(255) not null,
+	`longitude` decimal(10,8),
+	`latitude` decimal(8,6),
+	primary key (`addressID`),
+);
+
+create table if not exists `Supplier` (
+	`supplierID` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`phone` int,
+	`timeJoined` timestamp default CURRENT_TIMESTAMP,
+	`addressID` int NOT NULL,
+	PRIMARY KEY (`supplierID`),
+	FOREIGN KEY (`addressID`) REFERENCES `Address`(`addressID`) ON DELETE RESTRICT
+);
+
 create table if not exists `Product` (
 	`productID` int not null auto_increment unique,
 	`stock` int not null,
@@ -13,13 +36,15 @@ create table if not exists `Product` (
 	`timeListed` timestamp not null default current_timestamp,
 	`brand` varchar(64),
 	`showProduct` boolean default true,
+	`supplierID` int not null,
 	`pictures` BLOB, /* still needs some work */
 	`material` varchar(64),
 	`capacityLitres` decimal(4,1),
 	`warrantyMonths` int(3),
 	`serialNumber` varchar(64),
 	`popularity` int(3) default 0,
-	primary key (`productID`)
+	primary key (`productID`),
+	foreign key (`supplierID`) references `Supplier` (`supplierID`) on delete restrict
 );
 
 create table if not exists `Category` (
@@ -38,12 +63,51 @@ create table if not exists `CategoryCategorizesProduct` (
 	foreign key (`productID`) references `Product` (`productID`) on delete cascade
 );
 
+create table if not exists `User` (
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL, -- enforced format
+	`username` varchar(255) NOT NULL, /* covering constraint TODO + needs to be encrypted */
+	`password` varchar(255) NOT NULL, -- needs to be encrypted
+	PRIMARY KEY (`username`),
+    CONSTRAINT `chk_valid_email` CHECK (`email` LIKE '%_@__%.__%')
+);
+
+create table if not exists `ProductManager` ( -- ProductManager IS A User
+	`username` varchar(255) NOT NULL,
+	`supplierID` int NOT NULL,
+	FOREIGN KEY (`username`) REFERENCES User(`username`),
+	FOREIGN KEY (`supplierID`) REFERENCES Supplier(`supplierID`)
+);
+
+create table if not exists `SalesManager` ( -- SalesManager IS A User
+	`username` varchar(255) NOT NULL,
+	`supplierID` int NOT NULL,
+	FOREIGN KEY (`username`) REFERENCES User(`username`),
+	FOREIGN KEY (`supplierID`) REFERENCES Supplier(`supplierID`)
+);
+
+create table if not exists `ProdManagerContactsCourier` (
+    `deliveryAddressID` int NOT NULL,
+    `capacityPoints` int NOT NULL,
+    `productManagerUsername` varchar(255),
+    FOREIGN KEY (`deliveryAddressID`) REFERENCES `Address`(`addressID`) ON DELETE RESTRICT,
+    foreign key (`courierID`) references `Courier`(`courierID`) on delete restrict,
+    foreign key (`productManagerUsername`) references `ProductManager`(`username`) on delete restrict,
+    primary key (`courierID`, `productManagerUsername`)
+);
+
 create table `Customer` (
     `customerID` int not null auto_increment unique,
+    `username` varchar(255) not null,
+    `addressID` int not null,
     `timeJoined` timestamp not null default current_timestamp,
     `phone` varchar(15),
     `taxID` varchar(20),
-    primary key (`customerID`)
+    `wishlist` int,
+    primary key (`customerID`),
+    foreign key (`wishlist`) references Wishlist(`wishlistID`) on delete restrict,
+    foreign key (`username`) references User(`username`) on delete cascade,
+    foreign key (`addressID`) references Address(`addressID`) on delete restrict
 );
 
 create table `Wishlist` (
@@ -54,19 +118,6 @@ create table `Wishlist` (
     primary key (`wishlistID`, `customerID`, `productID`),
     foreign key (`customerID`) references Customer(`customerID`) on delete cascade,
     foreign key (`productID`) references Product(`productID`) on delete cascade
-);
-
-create table `Address` (
-	`addressID` int not null auto_increment unique,
-	`addressTitle` varchar(64) not null,
-	`country` varchar(64) not null,
-	`city` varchar(64) not null,
-	`province` varchar(64),
-	`zipCode` varchar(10) not null,
-	`streetAddress` varchar(255) not null,
-	`longitude` decimal(10,8),
-	`latitude` decimal(8,6),
-	primary key (`addressID`),
 );
 
 create table `BillingInfo` (
@@ -82,58 +133,82 @@ create table `BillingInfo` (
     	check (`creditCardEXP` regexp '^(0[1-9]|1[0-2])/([0-9]{2})$') -- Enforce mm/yy format
 );
 	
-
-
 create table if not exists `Courier` (
-    `CourierID` int NOT NULL AUTO_INCREMENT,
-    `Name` varchar(255),
-    `Phone` int NOT NULL,
-    `Email` varchar(255) NOT NULL,
-    `TimeJoined` timestamp default CURRENT_TIMESTAMP NOT NULL,
-    `Capacity` int default 0,
+    `courierID` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(255),
+    `phone` int NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `timeJoined` timestamp default CURRENT_TIMESTAMP NOT NULL,
+    `capacity` int default 0,
 	`addressID` int NOT NULL,
-    PRIMARY KEY (`CourierID`),
+    PRIMARY KEY (`courierID`),
 	FOREIGN KEY (`addressID`) REFERENCES `Address`(`addressID`) ON DELETE RESTRICT
 );
 
 create table if not exists `DeliveryRegion` (
-    `RegionID` int NOT NULL AUTO_INCREMENT,
-    `Name` varchar(255) NOT NULL,
-    `Population` int,
+    `regionID` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `population` int,
     `SEIFA` int,
-    PRIMARY KEY (`RegionID`)
+    PRIMARY KEY (`regionID`)
 );
 
 create table if not exists `CourierDeliversToDeliveryRegion` (
-    `CourierID` int NOT NULL,
-    `RegionID` int NOT NULL,
-    `DeliveryCost` int NOT NULL,
-    PRIMARY KEY (`CourierID`, `RegionID`),
-    FOREIGN KEY (`CourierID`) REFERENCES `Courier`(`CourierID`) ON DELETE CASCADE,    -- dont want to update IDs
-    FOREIGN KEY (`RegionID`) REFERENCES `DeliveryRegion`(`RegionID`) ON DELETE CASCADE
+    `courierID` int NOT NULL, /* participation constraint still needs to be done */
+    `regionID` int NOT NULL,
+    `reliveryCost` int NOT NULL,
+    PRIMARY KEY (`courierID`, `regionID`),
+    FOREIGN KEY (`courierID`) REFERENCES `Courier`(`courierID`) ON DELETE CASCADE,    -- dont want to update IDs
+    FOREIGN KEY (`regionID`) REFERENCES `DeliveryRegion`(`regionID`) ON DELETE CASCADE
 );
 
-
-create table if not exists `User` (
-	`name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL, -- enforced format
-	`username` varchar(255) NOT NULL, -- needs to be encrypted
-	`password` varchar(255) NOT NULL, -- needs to be encrypted
-	PRIMARY KEY (`username`),
-    CONSTRAINT `chk_valid_email` CHECK (`email` LIKE '%_@__%.__%')
+create table if not exists `Cart` (
+	`cartID` int NOT NULL AUTO_INCREMENT,
+	`totalPrice` real NOT NULL,
+	`numProducts` int NOT NULL,
+	`fingerprint` varchar(255) NOT NULL,
+	`temporary` boolean NOT NULL,
+	`customerID` int,
+	PRIMARY KEY (`cartID`),
+	FOREIGN KEY (`customerID`) REFERENCES `Customer`(`customerID`) ON DELETE CASCADE
 );
 
-create table if not exists `ProductManager` ( -- ProductManager IS A User
-	`username` varchar(255) NOT NULL PRIMARY KEY REFERENCES User(`username`)
+create table if not exists `CartContainsProduct` (
+	`cartID` int NOT NULL,  /* participation constraint still needs to be done */
+	`productID` int,
+	PRIMARY KEY (`cartID`, `productID`),
+	FOREIGN KEY (`cartID`) REFERENCES `Cart`(`cartID`) ON DELETE CASCADE,
+	FOREIGN KEY (`productID`) REFERENCES `Product`(`productID`) ON DELETE CASCADE
 );
 
-create table if not exists `SalesManager` ( -- SalesManager IS A User
-		`username` varchar(255) NOT NULL PRIMARY KEY REFERENCES User(`username`)
+create table if not exists `Order` (
+	`orderID` int NOT NULL AUTO_INCREMENT,
+	`orderNumber` int NOT NULL,
+	`timeOrdered` timestamp default CURRENT_TIMESTAMP NOT NULL,
+	`totalPrice` real NOT NULL,
+	`deliveryID` int NOT NULL,				-- Delivery is a relationship set	
+	`deliveryStatus` varchar(255) NOT NULL,
+	`deliveryAddressID` int NOT NULL,
+	`estimatedArrival` date NOT NULL,
+	`courierID` int NOT NULL,
+	`cartID` int NOT NULL,
+	PRIMARY KEY (`orderID`),
+	foreign key (`cartID`) references `Cart`(`cartID`) on delete restrict,
+	FOREIGN KEY (`deliveryAddressID`) REFERENCES `Address`(`addressID`) ON DELETE RESTRICT,
+	FOREIGN KEY (`courierID`) REFERENCES `Courier`(`courierID`) ON DELETE RESTRICT,
 );
 
-create table if not exists `ProdManagerContactsCourier` ( --Going to get to this tomorrow -Areeb :)
+create table if not exists `OrderOrderItemsProduct` (
+	`orderID` int NOT NULL, /* participation constraint still needs to be done */
+	`productID` int,
+	`quantity` int NOT NULL,
+	`purchasePrice` real NOT NULL,
+	PRIMARY KEY (`orderID`, `productID`),
+	FOREIGN KEY (`orderID`) REFERENCES `Order`(`orderID`) ON DELETE CASCADE,
+	FOREIGN KEY (`productID`) REFERENCES `Product`(`productID`) ON DELETE CASCADE
+);
 
-)
+/* what needs to be done now is reviews, returns and all the related tables to those */
 
 -- not working properly yet, "but i think this is the proper way" (particpation constraint)
 -- DELIMITER //
