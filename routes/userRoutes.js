b2ef64dd9ab6a2cebf2e9702922c70b0df65d64d
featherybatarde <areebkamal5@gmail.com>
2024-11-16 10:06:09 +0300

    purged all the new js and json files and folders so that we can start building the apis from scratch using mysql library

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {getAllUsers, getAllUsersStatic} = require('../controllers/userController')
-
-router.route('/').get(getAllUsers)
-router.route('/static').get(getAllUsersStatic)
-
-module.exports = router
