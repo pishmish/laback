1dfab717742bcffbce171b138656d2a94e8849e7
Nuh Al-Sharafi <103935821+N-alsharafi@users.noreply.github.com>
2024-11-19 15:22:11 +0300

    adjusted create and update to add support for colors

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -28,8 +28,8 @@ const getProductById = async (req, res) => {
 
 const createProduct = async (req, res) => {
   try {
-    let sql = 'INSERT INTO `Product` (stock ,name, unitPrice, overallRating, discountPercentage, description, brand, supplierID, material, capacityLitres, warrantyMonths, serialNumber, popularity) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const [results, fields] = await db.promise().query(sql, [req.body.stock, req.body.name, req.body.unitPrice, req.body.overallRating, req.body.discountPercentage, req.body.description, req.body.brand, req.body.supplierID, req.body.material, req.body.capacityLitres, req.body.warrantyMonths, req.body.serialNumber, req.body.popularity]);
+    let sql = 'INSERT INTO `Product` (stock ,name, unitPrice, overallRating, discountPercentage, description, brand, color, supplierID, material, capacityLitres, warrantyMonths, serialNumber, popularity) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const [results, fields] = await db.promise().query(sql, [req.body.stock, req.body.name, req.body.unitPrice, req.body.overallRating, req.body.discountPercentage, req.body.description, req.body.brand, req.body.color, req.body.supplierID, req.body.material, req.body.capacityLitres, req.body.warrantyMonths, req.body.serialNumber, req.body.popularity]);
     res.status(200).json({msg: "Product created"});
   } catch(err) {
     console.log(err);
@@ -40,8 +40,8 @@ const createProduct = async (req, res) => {
 const updateProduct = async (req, res) => {
   //this is a general update function, its meant to be implemented in other functions after user authentication
   try {
-    let sql = 'UPDATE `Product` SET stock = ?, name = ?, unitPrice = ?, overallRating = ?, discountPercentage = ?, description = ?, brand = ?, supplierID = ?, material = ?, capacityLitres = ?, warrantyMonths = ?, serialNumber = ?, popularity = ? WHERE productID = ?';
-    const [results, fields] = await db.promise().query(sql, [req.body.stock, req.body.name, req.body.unitPrice, req.body.overallRating, req.body.discountPercentage, req.body.description, req.body.brand, req.body.supplierID, req.body.material, req.body.capacityLitres, req.body.warrantyMonths, req.body.serialNumber, req.body.popularity, req.params.id]);
+    let sql = 'UPDATE `Product` SET stock = ?, name = ?, unitPrice = ?, overallRating = ?, discountPercentage = ?, description = ?, brand = ?, color = ?, supplierID = ?, material = ?, capacityLitres = ?, warrantyMonths = ?, serialNumber = ?, popularity = ? WHERE productID = ?';
+    const [results, fields] = await db.promise().query(sql, [req.body.stock, req.body.name, req.body.unitPrice, req.body.overallRating, req.body.discountPercentage, req.body.description, req.body.brand, req.body.color, req.body.supplierID, req.body.material, req.body.capacityLitres, req.body.warrantyMonths, req.body.serialNumber, req.body.popularity, req.params.id]);
     res.status(200).json({msg: "Product updated"});
   } catch(err) {
     console.log(err);
