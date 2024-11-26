79337b51ff1423bbfd9f4758ffea9a52f9ce211c
Nuh Al-Sharafi <103935821+N-alsharafi@users.noreply.github.com>
2024-11-26 18:06:18 +0300

    added ability to retrieve all products for a category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -55,10 +55,36 @@ const deleteCategory = async (req, res) => {
   }
 }
 
+const getCategoryProducts = async (req, res) => {
+  try {
+    //find the category id
+    let sql3 = 'SELECT * FROM `Category` WHERE name = ?';
+    const [category, fields3] = await db.promise().query(sql3, [req.params.name]);
+
+
+    //get the products of the category
+    let sql2 = 'SELECT * FROM `CategoryCategorizesProduct` WHERE categoryID = ?';
+    const [results2, fields2] = await db.promise().query(sql2, [category[0].categoryID]);
+
+    //get the products
+    finalResults = [];
+    for (let i = 0; i < results2.length; i++) {
+      sql = 'SELECT * FROM `Product` WHERE productID = ?';
+      const [product, fields] = await db.promise().query(sql, [results2[i].productID]);
+      finalResults.push(product[0]);
+    }
+    return res.status(200).json(finalResults);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({msg: "Error retrieving products"});
+  }
+}
+
 module.exports = {
   getAllCategories,
   getCategoryByName,
   createCategory,
   updateCategory,
-  deleteCategory
+  deleteCategory,
+  getCategoryProducts
 }
