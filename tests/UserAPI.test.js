8a420e27a76b4758f48131c117620fb6b61ed014
Musab Ahmed Khan <83080859+MAKhan22@users.noreply.github.com>
2024-11-27 13:49:02 +0300

    added 25 new unit tests for address, cart, user, store apis

---
+++ tests/UserAPI.test.js
@@ -0,0 +1,30 @@
+const supertest = require('supertest');
+const app = require('../src/server'); // Adjust the path as needed
+// const db = require('../src/config/database'); // Adjust the path as needed
+// const e = require('express');
+let server;
+let request;
+
+beforeAll((done) => {
+  server = app.listen(done); // Start the app
+  request = supertest(server); // Attach Supertest to the server
+});
+
+afterAll((done) => {
+  server.close(done); // Close the server after all tests
+//   db.end(done); // Close the database connection
+});
+
+describe("GET /user", () => {
+    it("should respond to the GET method", async () => {
+        const response = await request.get("/user");
+        expect(response.status).toBe(200);
+        expect(response.body).toBeDefined();
+        expect(response.text).toEqual("User API, welcome!");
+    });
+    
+    it("should return a 404 status for an invalid route", async () => {
+        const response = await request.get("/user/invalid-route");
+        expect(response.status).toBe(404);
+    });
+});
