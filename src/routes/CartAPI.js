dd7672d44af20083631eda5ffc881ea368096a34
Cem <cemgorkem@sabanciuniv.edu>
2024-11-29 21:27:12 +0300

    cartcontains apis added

--- src/routes/CartAPI.js
+++ src/routes/CartAPI.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 // Import cart controller
 const cartController = require('../controllers/cart');
-// const cartContainsProductController = require('../controllers/cartContainsProduct');
+const cartContainsProductController = require('../controllers/cartContainsProduct'); // Yeni controller import
 
-//sample sanity route
+// Sample sanity route
 router.get('/', (req, res) => {
     res.send('Cart API, welcome!');
 });
@@ -14,11 +14,6 @@ router.get('/', (req, res) => {
 router.get('/fetch', (req, res) => {
     return cartController.getOrCreateCart(req, res);
 });
-
-// // Merge carts on login using cookies and customerID
-// router.post('/merge', async (req, res) => {
-//     return cartController.mergeCarts(req, res);
-// });
 
 // Add product to cart (creation or insertion) using cookies (while creating a new cart if necessary)
 router.post('/product/:productID', (req, res) => {
@@ -30,14 +25,14 @@ router.put('/product/:productID', (req, res) => {
     return cartController.removeProductFromCart(req, res);
 });
 
-// Delete product from cart cart using cookies
+// Delete product from cart using cookies
 router.delete('/product/:productID', (req, res) => {
     return cartController.deleteProductFromCart(req, res);
 });
 
-// router.get('/products', (req, res) => {
-//     return cartContainsProductController.getCartProducts(req, res);
-// });
+// Fetch products in the cart
+router.get('/products', (req, res) => {
+    return cartContainsProductController.getCartProducts(req, res); // Yeni API endpoint
+});
 
 module.exports = router;
-
