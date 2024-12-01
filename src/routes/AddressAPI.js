491bb1c1174b5c1e49a522f4ba630d8cc2453363
Nuh Al-Sharafi <103935821+N-alsharafi@users.noreply.github.com>
2024-12-01 21:07:37 +0300

    implemented address API, commented the cron in server.js cuz its irrelevant at this stage, updated api-endpoints with newest endpoints

--- src/routes/AddressAPI.js
+++ src/routes/AddressAPI.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//Middleware
+const { authenticateToken, authenticateRole } = require('../middleware/auth-handler');
+
 //import controllers
 const addressController = require('../controllers/address');
 
@@ -11,31 +14,33 @@ router.get('/', (req, res) => {
     res.send('Address API, welcome!');
 });
 
-// // Section : General Address Routes
-// router.get('/:addressid', (req, res) => {
-//     //TODO: require authentication
-//     return addressController.getAddress(req, res);
-// });
+// Section : General Address Routes
+// this route is to get addresses associated to orders and billing info, not properly secured, depends on the implementation.
+router.get('/id/:addressid', authenticateToken, (req, res) => {
+    return addressController.getAddress(req, res);
+});
 
-// router.get('/:username', (req, res) => {
-//     //TODO: require authentication
-//     return addressController.getUserAddress(req, res);
-// });
+//this is for product manager to find addresses of customers for delivery
+router.get('/uname/:username', authenticateToken, authenticateRole('productManager'), (req, res) => {
+    return addressController.getUserAddress(req, res);
+});
 
-// router.post('/newaddress', (req, res) => {
-//     //TODO: require authentication
-//     return addressController.createAddress(req, res);
-// });
+router.get('/personal', authenticateToken, authenticateRole('customer'), (req, res) => {
+    return addressController.getPersonalAddress(req, res);
+});
 
-// router.put('/:addressid', (req, res) => {
-//     //TODO: require authentication
-//     return addressController.updateAddress(req, res);
-// });
+router.post('/newaddress', authenticateToken, (req, res) => {
+    return addressController.createAddress(req, res);
+});
 
-// router.delete('/:addressid', (req, res) => {
-//     //TODO: require authentication
-//     return addressController.deleteAddress(req, res);
-// });
+// also not secure, depends on implementation
+router.put('/:addressid', authenticateToken, (req, res) => {
+    return addressController.updateAddress(req, res);
+});
+
+router.delete('/:addressid', authenticateToken, (req, res) => {
+    return addressController.deleteAddress(req, res);
+});
 
 module.exports = router;
 
