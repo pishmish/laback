cd9ed32fb9f41681db2c66c7f8a40e2f784959ab
Nuh Al-Sharafi <103935821+N-alsharafi@users.noreply.github.com>
2024-12-02 13:22:19 +0300

    finalized delivery and moved it to delivery.js, starting orders now

--- src/routes/DeliveryAPI.js
+++ src/routes/DeliveryAPI.js
@@ -1,38 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+//Middleware
+const { authenticateToken, authenticateRole } = require('../middleware/auth-handler');
+
 // import controller:
-const orderController = require('../controllers/order');
+const deliveryController = require('../controllers/delivery');
 
 // Routes:
+// TODO: authentication?
 
 //sample sanity route
 router.get('/', (req, res) => {
     res.send('Delivery API, welcome!');
-  });
-
-router.get('/estimate/:id', (req,res) => {
-  return orderController.getEstimatedTimeById(req, res);
 });
 
-router.post('/order', (req, res) => {
-  return orderController.createOrder(req, res);
+router.get('/estimate/:id', authenticateToken, (req,res) => {
+  return deliveryController.getEstimatedTimeById(req, res);
 });
 
-router.get('/order', (req,res) => {
-  return orderController.getAllOrders(req, res);
+router.put('/estimate/:id', authenticateToken, (req, res) => {
+  return deliveryController.updateEstimatedTime(req, res);
 });
 
-router.get('/order/:id', (req,res) => {
-  return orderController.getOrderById(req, res);
+router.get('/order/courier/:courierid', authenticateToken, (req,res) => {
+  return deliveryController.getOrderByCourierId(req, res);
 });
 
-router.put('/order/status', (req, res) => {
-  return orderController.updateDeliveryStatus(req, res);
+router.get('/order/:id', authenticateToken, (req,res) => {
+  return deliveryController.getOrderById(req, res);
 });
 
-router.delete('/order/:id', (req, res) => {
-  return orderController.deleteOrder(req, res);
+router.put('/order/:id/status', authenticateToken, (req, res) => {
+  return deliveryController.updateDeliveryStatus(req, res);
+});
+
+router.get('/order/:id/status', authenticateToken, (req, res) => {
+  return deliveryController.getDeliveryStatus(req, res);
 });
 
 module.exports = router;
