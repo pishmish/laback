6a920660cf90fffeb8b6d1abaad32c564ed3a136
Nuh Al-Sharafi <103935821+N-alsharafi@users.noreply.github.com>
2024-12-12 23:52:33 +0300

    returns API complete

---
+++ src/routes/ReturnsAPI.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+//Middleware
+const {authenticateToken, authenticateRole} = require('../middleware/auth-handler');
+
+//import controller
+const returnsController = require('../controllers/returns');
+
+//Routes
+
+
+//sample sanity route
+router.get('/', (req, res) => {
+    res.send('Returns API is working');
+});
+
+router.get('/all', authenticateToken, authenticateRole('salesManager'), (req, res) => {
+    return returnsController.getAllReturns(req, res);
+});
+
+router.get('/request/:id', authenticateToken, authenticateRole('salesManager'), (req, res) => {
+    return returnsController.getRequest(req, res);
+});
+
+router.get('/request/:id/status', authenticateToken, (req, res) => {
+    return returnsController.getRequestStatus(req, res);
+});
+
+router.post('/newrequest', authenticateToken, authenticateRole('customer'), (req, res) => {
+    return returnsController.newRequest(req, res);
+});
+
+router.put('/request/:id', authenticateToken, (req, res) => {
+    return returnsController.updateRequest(req, res);
+});
+
+router.put('/request/:id/status', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
+    return returnsController.updateRequestStatus(req, res);
+});
+
+router.delete('/request/:id', authenticateToken, authenticateRole('customer'), (req, res) => {
+    return returnsController.deleteRequest(req, res);
+});
+
+module.exports = router;
