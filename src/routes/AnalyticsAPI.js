43dcc8977a61970c318334832bba0812d5edc1a8
Musab Ahmed Khan <83080859+MAKhan22@users.noreply.github.com>
2024-12-12 11:48:42 +0300

    modified analyticsAPI to include authentication now

--- src/routes/AnalyticsAPI.js
+++ src/routes/AnalyticsAPI.js
@@ -1,89 +1,90 @@
 const express = require('express');
 const router = express.Router();
 const analyticsController = require('../controllers/analytics');
+const { authenticateToken, authenticateRole } = require('../middleware/auth-handler');
 
 router.get('/', (req, res) => {
     res.send('Analytics API, welcome!');
 });
 
 //gets sales data (all products) for each day
-router.get('/sales', (req, res) => {
+router.get('/sales', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getAllSales(req, res);
 });
 
 //gets sales data (all products) for each month
-router.get('/sales/monthly', (req, res) => {
+router.get('/sales/monthly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getMonthlySales(req, res);
 });
 
 //gets sales data (all products) for each quarter
-router.get('/sales/quarterly', (req, res) => {
+router.get('/sales/quarterly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getQuarterlySales(req, res);
 });
 
 //gets sales data (all products) for each year
-router.get('/sales/yearly', (req, res) => {
+router.get('/sales/yearly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getYearlySales(req, res);
 });
 
 //localhost:5001/analytics/sales/comparison?start1=2024-01-01&end1=2024-12-30&start2=2023-01-01&end2=2023-06-30
 //gets the sales comparison between two time periods
-router.get('/sales/comparison', (req, res) => {
+router.get('/sales/comparison', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getSalesComparison(req, res);
 });
 
 //gets the sales (a specific product) for each day
-router.get('/sales/product/:productid', (req, res) => {
+router.get('/sales/product/:productid', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getProductSales(req, res);
 });
 
 //get the sales (a specific product) for each month
-router.get('/sales/product/:productid/monthly', (req, res) => {
+router.get('/sales/product/:productid/monthly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getProductMonthlySales(req, res);
 });
 
 //get the sales (a specific product) for each quarter
-router.get('/sales/product/:productid/quarterly', (req, res) => {
+router.get('/sales/product/:productid/quarterly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getProductQuarterlySales(req, res);
 }); 
 
 //get the sales (a specific product) for each year
-router.get('/sales/product/:productid/yearly', (req, res) => {
+router.get('/sales/product/:productid/yearly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getProductYearlySales(req, res);
 });
 
 //gets the sales comparison (a specific product) between two time periods
-router.get('/sales/product/:productid/comparison', (req, res) => {
+router.get('/sales/product/:productid/comparison', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getProductSalesComparison(req, res);
 });
 
 //CAN BE USED FOR BOTH MAIN CATEGORIES AND SUB CATEGORIES
 //gets the sales for a category/subcategory for each day
-router.get('/sales/category/:categoryid', (req, res) => {
+router.get('/sales/category/:categoryid', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getCategorySales(req, res);
 });
 
 //CAN BE USED FOR BOTH MAIN CATEGORIES AND SUB CATEGORIES
 //gets the sales for a category/subcategory for each month
-router.get('/sales/category/:categoryid/monthly', (req, res) => {
+router.get('/sales/category/:categoryid/monthly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getCategoryMonthlySales(req, res);
 });
 
 //CAN BE USED FOR BOTH MAIN CATEGORIES AND SUB CATEGORIES
 //gets the sales for a category/subcategory for each quarter
-router.get('/sales/category/:categoryid/quarterly', (req, res) => {
+router.get('/sales/category/:categoryid/quarterly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getCategoryQuarterlySales(req, res);
 });
 
 //CAN BE USED FOR BOTH MAIN CATEGORIES AND SUB CATEGORIES
 //gets the sales for a category/subcategory for each year
-router.get('/sales/category/:categoryid/yearly', (req, res) => {
+router.get('/sales/category/:categoryid/yearly', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getCategoryYearlySales(req, res);
 });
 
 //CAN BE USED FOR BOTH MAIN CATEGORIES AND SUB CATEGORIES
 //gets the sales comparison for a category/subcategory between two time periods
-router.get('/sales/category/:categoryid/comparison', (req, res) => {
+router.get('/sales/category/:categoryid/comparison', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getCategorySalesComparison(req, res);
 });
 
@@ -93,47 +94,47 @@ router.get('/sales/category/:categoryid/comparison', (req, res) => {
 // });
 
 //gets the sales for each province
-router.get('/salesByProvince', (req, res) => {
+router.get('/salesByProvince', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getSalesByProvince(req, res);
 });
 
 //gets the sales for each city
-router.get('/salesByCity', (req, res) => {
+router.get('/salesByCity', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getSalesByCity(req, res);
 });
 
 //gets the sales for each country
-router.get('/salesByCountry', (req, res) => {
+router.get('/salesByCountry', authenticateToken, authenticateRole('salesManager'), (req, res) => {
     return analyticsController.getSalesByCountry(req, res);
 });
 
-//get effectiveness of discount ranges (revenue generated by each discount range)
-router.get('/discountEffectiveness', (req, res) => {
-    return analyticsController.getDiscountEffectiveness(req, res);
-});
-
 //gets the products that are below the stock threshold (set to 10 for now)
-router.get('/product/lowStock', (req, res) => {
+router.get('/product/lowStock', authenticateToken, authenticateRole('productManager'), (req, res) => {
     return analyticsController.getLowStockProducts(req, res);
 });
 
 //gets top 10 best sellers based on the total units sold
-router.get('/product/bestSellers', (req, res) => {
+router.get('/product/bestSellers', authenticateToken, authenticateRole('productManager'), (req, res) => {
     return analyticsController.getBestSellers(req, res);
 });
 
 //gets top 10 most viewed products based on the popularity
-router.get('/product/mostViewed', (req, res) => {
+router.get('/product/mostViewed', authenticateToken, authenticateRole('productManager'), (req, res) => {
     return analyticsController.getMostViewedProducts(req, res);
 });
 
+//get effectiveness of discount ranges (revenue generated by each discount range)
+router.get('/discountEffectiveness', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
+    return analyticsController.getDiscountEffectiveness(req, res);
+});
+
 //get the top buyers based on the total amount spent
-router.get('/user/topBuyers', (req, res) => {
+router.get('/user/topBuyers', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
     return analyticsController.getTopBuyers(req, res);
 });
 
 //get churn rate(percentage of users who have not made a purchase in the last 12 months)
-router.get('/user/churnRate', (req, res) => {
+router.get('/user/churnRate', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
     return analyticsController.getChurnRate(req, res);
 });
 
@@ -144,22 +145,22 @@ router.get('/user/churnRate', (req, res) => {
 // });
 
 //get change in quantity / stock of products by product manager for each day
-router.get('/inventory', (req, res) => {
+router.get('/inventory', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
     return analyticsController.getInventoryTrend(req, res);
 });
 
 //get products that need to be reordered (qty <= 10 units)
-router.get('/inventory/reorder', (req, res) => {
+router.get('/inventory/reorder', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
     return analyticsController.getReorderNeeds(req, res);
 });
 
 //get number of returns, qty sold for each product
-router.get('/returns/summary', (req, res) => {
+router.get('/returns/summary', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
     return analyticsController.getReturnsSummary(req, res);
 });
 
 //get count of each reason for return
-router.get('/returns/byReason', (req, res) => {
+router.get('/returns/byReason', authenticateToken, authenticateRole(['salesManager', 'productManager']), (req, res) => {
     return analyticsController.getReturnsByReason(req, res);
 });
 
